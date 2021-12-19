a2a447eb9e4f160d209d031de61775eae0c04d99
Julian <julianstamb@gmail.com>
2021-12-19 20:15:18 +0100

    adds const where possible in shaders

--- Newtonian-Particle-Simulator/res/shaders/particles/vertex.glsl
+++ Newtonian-Particle-Simulator/res/shaders/particles/vertex.glsl
@@ -28,19 +28,19 @@ void main()
 {
     Particle particle = ssbo.particles[gl_VertexID];
 
-    vec3 toMass = pointOfMass - particle.Position;
-    float distSqured = max(dot(toMass, toMass), EPSILON * EPSILON);
+    const vec3 toMass = pointOfMass - particle.Position;
+    const float distSqured = max(dot(toMass, toMass), EPSILON * EPSILON);
     
-    vec3 acceleration = (176.0 * toMass * isRunning * isActive) / distSqured;
+    const vec3 acceleration = (176.0 * toMass * isRunning * isActive) / distSqured;
     particle.Velocity *= mix(1.0, exp(DRAG_COEF * dT), isRunning); // https://stackoverflow.com/questions/61812575/which-formula-to-use-for-drag-simulation-each-frame
     particle.Position += (dT * particle.Velocity + 0.5 * acceleration * dT * dT) * isRunning;
     particle.Velocity += acceleration * dT;
     ssbo.particles[gl_VertexID] = particle;
 
 
-    float red = 0.0045 * dot(particle.Velocity, particle.Velocity);
-    float green = clamp(0.08 * max(particle.Velocity.x, max(particle.Velocity.y, particle.Velocity.z)), 0.2, 0.5);
-    float blue = 0.7 - red;
+    const float red = 0.0045 * dot(particle.Velocity, particle.Velocity);
+    const float green = clamp(0.08 * max(particle.Velocity.x, max(particle.Velocity.y, particle.Velocity.z)), 0.2, 0.5);
+    const float blue = 0.7 - red;
 
     outData.Color = vec4(red, green, blue, 0.25);
     gl_Position = projViewMatrix * vec4(particle.Position, 1.0);
